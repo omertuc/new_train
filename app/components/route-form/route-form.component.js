cde801267759bec519f01a922b584760daceba96
omertuc <omer@omer.omer>
2017-02-23 20:51:48 +0200

    Removed jQ bullshit, added angular-material. Form looks better now

--- app/components/route-form/route-form.component.js
+++ app/components/route-form/route-form.component.js
@@ -1,15 +1,25 @@
 'use strict';
 
-angular.module('myApp.routeForm').component('routeForm', {
+angular.module('myApp.routeForm', ['ngMaterial', 'ngMessages']).component('routeForm', {
     templateUrl: 'components/route-form/route-form.template.html',
-    controller: ['stationService', '$http', function (stationService, $עד_מתי) {
-        stationService.getStations().then(({data}) => this.stations = data);
+    controller: ['stationService', '$http', function (stationService, $http) {
+        this.myDate = new Date();
 
-        this.initializeDatepicker = function() {
-            let datePicker = $("#datepick");
-            datePicker.datepicker({dateFormat: "yymmdd"});
-            datePicker.datepicker("setDate", new Date());
-        };
+        this.minDate = new Date(
+            this.myDate.getFullYear(),
+            this.myDate.getMonth(),
+            this.myDate.getDate()
+        );
+
+        this.maxDate = new Date(
+            this.myDate.getFullYear(),
+            this.myDate.getMonth() + 2,
+            this.myDate.getDate()
+        );
+
+        this.isOpen = false;
+
+        stationService.getStations().then(({data}) => this.stations = data);
 
         this.isValidStation = function (stationId) {
             return this.stations.map((stationObject) => Number(stationObject.Id))
@@ -32,18 +42,8 @@ angular.module('myApp.routeForm').component('routeForm', {
                 return;
             }
 
-            $עד_מתי.get(`https://www.rail.co.il/apiinfo/api/Plan/GetRoutes?OId=${this.origin}&TId=${this.destination}&Date=${this.datePickerInput}&Hour=0000`).then
+            $http.get(`https://www.rail.co.il/apiinfo/api/Plan/GetRoutes?OId=${this.origin}&TId=${this.destination}&Date=${this.datePickerInput}&Hour=0000`).then
             (({data}) => console.log(data));
-        };
-
-        this.nowClickHandler = function () {
-            console.log('ring');
-            this.initializeDatepicker();
-            this.submit();
-        };
-
-        this.otherTimeHandler = function () {
-            $("#datediv").toggleClass("unhidden hidden");
         };
 
         this.searchTimesHandler = function () {
