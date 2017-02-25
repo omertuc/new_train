cb61cac31888c711eee9a1e7df09e1e4c8dac18b
Avidor <avidor98@gmail.com>
2017-02-25 22:10:27 +0200

    Done some css styling and set date format to DD/MM/YYYY

--- app/app.js
+++ app/app.js
@@ -8,15 +8,20 @@ angular.module('myApp', [
     'myApp.stationSelectView',
     'myApp.stationList',
     'myApp.routeForm'
-]).config([
-    '$locationProvider',
-    '$routeProvider',
-    '$mdThemingProvider',
-    function ($locationProvider, $routeProvider, $mdThemingProvider) {
+])
+.config([
+    '$locationProvider', '$routeProvider', '$mdThemingProvider', '$mdDateLocaleProvider',
+    function ($locationProvider, $routeProvider, $mdThemingProvider, $mdDateLocaleProvider) {
         $locationProvider.hashPrefix('!');
         $routeProvider.otherwise({redirectTo: '/station-select'});
 
         $mdThemingProvider.theme('default').
             primaryPalette('blue').accentPalette('green').dark();
 
+        $mdDateLocaleProvider.formatDate = function(date) {
+          var day = date.getDate();
+          var monthIndex = date.getMonth();
+          var year = date.getFullYear();
+          return day + '/' + (monthIndex + 1) + '/' + year;
+        };
     }]);
